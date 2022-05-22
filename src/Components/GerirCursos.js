cb274ad8b2aa52d2ba0d62350004344145d6d264
Kleiton Da Rosa <krdelgado@uta.cv>
2022-05-22 20:32:09 +0100

    Modal-Formulario-Curso

--- src/Components/GerirCursos.js
+++ src/Components/GerirCursos.js
@@ -10,52 +10,54 @@ function GerirCursos() {
 
     return (
 
-
             <div>
                 {/*Modal Para o formulario criar Novo Curso*/}
                 <div className='modal-classe'>
-                    <button onClick={()=> setModalIsOpen(true)}>Adiconar Novo Curso</button>
+                    <button id='button-criarNovoCurso' onClick={()=> setModalIsOpen(true)}>Adiconar Novo Curso</button>
                     <Modal isOpen={modalIsOpen} onRequestClose={()=> setModalIsOpen(false)}>
-                        <form>
-                            <h2>Novo Curso</h2>
-                            <div>
-                                <div>
-                                    <label>Nome do Curso: </label>
-                                    <input type='text'></input>
-                                </div>
+
+                        <div className="container">
+                            <div className="box">
+                                <form>
+                                    <h2>Novo Curso</h2>
+                                    <div>
+                                        <div>
+                                            <label>Nome do Curso: </label>
+                                            <input type='text'></input>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div>
+                                            <label>Estado do Curso:</label>
+                                            <select>
+                                                <option value="">---Escolhe um Estado---</option>
+                                                <option value="activo">Activo</option>
+                                                <option value="desactivo">Desactivo</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label>Adicionar Coordenador</label>
+                                        <select>
+                                            <option value="">---Escolhe um Coordenador---</option>
+                                            <option value="Sidnei Cruz">Docente-Sidnei Cruz</option>
+                                            <option value="JandirMedina">Jandir Medina</option>
+                                            <option value="Jose Neves">Jose Neves</option>
+                                            <option value="Joao Do Monte">Joao Do Monte</option>
+                                        </select>
+                                    </div>
+
+
+
+
+                                </form>
                             </div>
+                        </div>
+                        <button className='button-guardar' onClick={()=>setModalIsOpen(false)}>Guardar</button>
+                        <button className='button-cancelar' onClick={()=> setModalIsOpen(false)}>Cancelar</button>
 
-                            <div>
-                                <div>
-                                    <label>Estado do Curso:</label>
-                                    <select>
-                                        <option value="">---Escolhe um Estado---</option>
-                                        <option value="activo">Activo</option>
-                                        <option value="desactivo">Desactivo</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div>
-                                <label>Adicionar Coordenador</label>
-                                <select>
-                                    <option value="">---Escolhe um Coordenador---</option>
-                                    <option value="Sidnei Cruz">Docente-Sidnei Cruz</option>
-                                    <option value="JandirMedina">Jandir Medina</option>
-                                    <option value="Jose Neves">Jose Neves</option>
-                                    <option value="Joao Do Monte">Joao Do Monte</option>
-                                </select>
-                            </div>
-
-
-
-
-                        </form>
-
-
-
-
-                        <button onClick={()=> setModalIsOpen(false)}>Cancelar</button>
                     </Modal>
 
 

b42113bdfd0d91d8fc6ac67f3a545fa885a16adb
Kleiton Da Rosa <krdelgado@uta.cv>
2022-05-23 08:47:08 +0100

    Formulario Modal Criar Cursos.

--- src/Components/GerirCursos.js
+++ src/Components/GerirCursos.js
@@ -1,9 +1,78 @@
 import "./GerirCursos.css"
+import React,{useState} from "react";
+import Modal from "react-modal";
 
 
-function retornar() {
-
+function GerirCursos() {
+    const [modalIsOpen , setModalIsOpen] = useState(false);
     return (
+        <div>
+            {/*Modal Para o formulario criar Novo Curso*/}
+            <div className='modal-classe'>
+                <button id='button-criarNovoCurso' onClick={()=> setModalIsOpen(true)}>Adiconar Novo Curso</button>
+                <Modal isOpen={modalIsOpen} onRequestClose={()=> setModalIsOpen(false)}>
+
+
+
+
+
+                    <div className='container-forms'>
+                        <div className='formulario'>
+
+                            <form>
+                                <h2>Novo Curso</h2>
+                                <div>
+                                    <div>
+                                        <label>Curso:      </label>
+                                        <input type='text'></input>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <label>Estado   </label>
+                                        <select>
+                                            <option value="">---Selecionar Estado---</option>
+                                            <option value="activo">Activo</option>
+                                            <option value="desactivo">Desactivo</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label>Coorde</label>
+                                    <select>
+                                        <option value="">---Selecionar um Coordenador---</option>
+                                        <option value="Sidnei Cruz">Docente-Sidnei Cruz</option>
+                                        <option value="JandirMedina">Jandir Medina</option>
+                                        <option value="Jose Neves">Jose Neves</option>
+                                        <option value="Joao Do Monte">Joao Do Monte</option>
+                                    </select>
+                                </div>
+                            </form>
+
+
+                        </div>
+                    </div>
+
+
+
+
+                    <div className='container-buttons'>
+                        <div className='button-guardar-cancelar'>
+                            <button className='button-guardar' onClick={()=>setModalIsOpen(false)}>Guardar</button>
+                            <button className='button-cancelar' onClick={()=> setModalIsOpen(false)}>Cancelar</button>
+                        </div>
+                    </div>
+
+
+                </Modal>
+
+
+
+
+            </div>
+            {/*---------------------------------------*/}
         <div id="tabela">
             <table>
                 <tr className="tableTop">
@@ -28,7 +97,8 @@ function retornar() {
                 </tr>
             </table>
         </div>
+            </div>
     )
 }
 
-export default retornar
+export default GerirCursos;

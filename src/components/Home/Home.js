41a7e418c41d7351d4578fc1fb730aa22c67d475
Walter Santos <waltercms14@gmail.com>
2022-06-06 08:54:08 -0100

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import "./Home.css"
 import history from "../../services/history";
 import Cabecalho from "./cabecalho/cabecalho";
-import Routes from "../../routes/routes";
+import Routes_ClickButtonsHome from "../../routes/Routes_ClickButtonsHome";
 
 export default function home() {
     return (
@@ -9,14 +9,11 @@ export default function home() {
             <Cabecalho/>
 
             <div className="buttonsNav">
-                <button className="btnNav" onClick={() => history.push("/administarmatriculas")}>Administar Matriculas
-                </button>
+                <button className="btnNav" onClick={() => history.push("/administarmatriculas")}>Administar Matriculas</button>
                 <button className="btnNav" onClick={() => history.push("/gerircursos")}>Gerir Cursos</button>
-                <button className="btnNav" onClick={() => history.push("/perfilutilizador")}>Perfil de Utilizador
-                </button>
+                <button className="btnNav" onClick={() => history.push("/perfilutilizador")}>Perfil de Utilizador</button>
             </div>
-
-            <Routes/>
+            <Routes_ClickButtonsHome/>
         </>
     )
 }
